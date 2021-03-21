086ef259926f171822e4936d10d2f5b46598c7a3
Kohei-Katashima <kkohei0325@gmail.com>
2021-03-21 15:14:19 +0900

    認証ページ作成

---
+++ backend/resources/js/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav class="navbar">
+    <RouterLink class="navbar__brand" to="/">
+      setlistApp
+    </RouterLink>
+    <div class="navbar__menu">
+      <div class="navbar__item">
+        <button class="button">
+          <i class="icon ion-md-add"></i>
+          セットリスト登録
+        </button>
+      </div>
+      <span class="navbar__item">
+        ユーザー名
+      </span>
+      <div class="navbar__item">
+        <RouterLink class="button button--link" to="/login">
+          ログイン / 新規登録
+        </RouterLink>
+      </div>
+    </div>
+  </nav>
+</template>
